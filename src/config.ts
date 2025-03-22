5002c6d32e3d706bdfc2ce04abf465107c847906
Jack-Liang <jack.liang.world@gmail.com>
2025-03-22 22:51:56 +0800

    feat: 添加MDX组件和博客相关组件
    添加了一系列MDX组件（如br、hr、i、p等）以及博客相关的组件（如post-list、post-list-item、post-content），用于支持博客内容的渲染和展示。同时添加了配置文件config.ts，定义了网站的基本信息和菜单配置。

---
+++ src/config.ts
@@ -0,0 +1,31 @@
+export const SITE_FAVICON = "/favicon.png";
+export const SITE_LOGO = "/avatar.png";
+export const SITE_TITLE = "Kik soft";
+export const SITE_DESCRIPTION =
+  "欢迎来到··Kik··soft，这是我的个人技术博客。在这里，我会分享软件开发、技术探索和个人成长的心得。通过文字记录学习历程，分享技术见解，期待与您一起探讨和成长。";
+export const MENUS = [
+  {
+    title: "Posts",
+    link: "/posts",
+    target: "_self",
+  },
+  {
+    title: "Tags",
+    link: "/tags",
+    target: "_self",
+  },
+  {
+    title: "Archive",
+    link: "/date",
+    target: "_self",
+  },
+  {
+    title: "GitHub",
+    link: "https://github.com/Jack-Liang",
+    target: "_blank",
+  },
+];
+
+export const FOOTER_CONTENT = "Let's build a 1B unicorn 🦄";
+
+export const GOOGLE_GTAG = "G-XXXXXXXXXX";
