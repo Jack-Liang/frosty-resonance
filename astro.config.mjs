ba4ccf89038c8743208f5c843e417911fbe3ef1a
Jack-Liang <jack.liang.world@gmail.com>
2025-03-23 00:18:12 +0800

    feat: 更新首页和关于页面内容，添加新依赖和配置
    - 在首页添加“关于我”标题和装修提示
    - 更新关于页面的英雄图片链接
    - 添加 React 和 Tailwind 依赖，并更新 Astro 配置
    - 删除旧的 Swift 相关博客文章
    - 调整博客布局中的英雄图片样式

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,28 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
 
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://kiksoft.net",
-  integrations: [mdx(), sitemap()],
+  // integrations: [mdx(), sitemap()],
+  integrations: [
+    mdx({
+      syntaxHighlight: "shiki",
+      shikiConfig: {
+        theme: "github-light",
+      },
+      // remarkPlugins: [remarkMath],
+      // rehypePlugins: [rehypeKatex],
+    }),
+    sitemap(),
+    tailwind(),
+    react(),
+  ],
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
